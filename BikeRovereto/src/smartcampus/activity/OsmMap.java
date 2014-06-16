c34a0f0fe13e1e3bd6b90e1d3c4f005a2a274960
DylanStenico <stenicodylan@gmail.com>
2014-06-16 10:03:10 +0200

    set switch from e-motion to all bikes methods and removed the MainActivity

--- BikeRovereto/src/smartcampus/activity/OsmMap.java
+++ BikeRovereto/src/smartcampus/activity/OsmMap.java
@@ -9,6 +9,7 @@ import smartcampus.util.BikeOverlayItem;
 import smartcampus.util.CustomInfoWindow;
 import smartcampus.util.MarkerOverlay;
 import smartcampus.util.StationOverlayItem;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -17,20 +18,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
 import eu.trentorise.smartcampus.bikerovereto.R;
-import eu.trentorise.smartcampus.osm.android.bonuspack.location.GeocoderNominatim;
 import eu.trentorise.smartcampus.osm.android.util.GeoPoint;
-import eu.trentorise.smartcampus.osm.android.util.OSMGeocoding;
 import eu.trentorise.smartcampus.osm.android.views.MapController;
 import eu.trentorise.smartcampus.osm.android.views.MapView;
 import eu.trentorise.smartcampus.osm.android.views.overlay.MyLocationOverlay;
 
 public class OsmMap extends ActionBarActivity
 {
+
+	// menu
+	private static final int MENU_ON_STATION_ID = 0;
+
 	// the view where the map is showed
 	private MapView mapView;
 
@@ -48,7 +53,10 @@ public class OsmMap extends ActionBarActivity
 
 	// marker for the bikes
 	MarkerOverlay<BikeOverlayItem> bikesMarkersOverlay;
-	
+
+	// switch
+	ToggleButton btSwitch;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -66,22 +74,20 @@ public class OsmMap extends ActionBarActivity
 		// to keep the display on
 		mapView.setKeepScreenOn(true);
 
-		// get the station from the parcels
-		stations = getIntent().getExtras().getParcelableArrayList("stations");
-		
-		// get the bikes from the parcels
-		bikes = getIntent().getExtras().getParcelableArrayList("bikes");
+		setUpPointers();
 
 		// stuff for my Location
 		myLoc = new MyLocationOverlay(getApplicationContext(), mapView);
 		myLoc.enableMyLocation();
 		myLoc.enableCompass();
 		mapView.getOverlays().add(myLoc);
-		
+
 		// add the markers on the mapView
 		addMarkers();
 
 		setActionBar();
+
+		setSwitch();
 	}
 
 	@Override
@@ -98,6 +104,32 @@ public class OsmMap extends ActionBarActivity
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		menu.add(Menu.NONE, MENU_ON_STATION_ID, Menu.NONE,
+				R.string.title_activity_stations);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+
+		super.onOptionsItemSelected(item);
+		
+		
+		switch (item.getItemId())
+		{
+		case MENU_ON_STATION_ID:
+			Intent listIntent = new Intent(getApplicationContext(),
+					StationsActivity.class);
+
+			listIntent.putParcelableArrayListExtra("stations", stations);
+
+			startActivity(listIntent);
+			
+			break;
+		default:
+			break;
+		}
 		return true;
 	}
 
@@ -117,7 +149,7 @@ public class OsmMap extends ActionBarActivity
 		{
 			markers.add(new BikeOverlayItem(bikes.get(i).getId(), "bike", bikes
 					.get(i).getPosition(), bikes.get(i)));
-			
+
 			Drawable markerImage = null;
 			markerImage = res.getDrawable(R.drawable.anarchich_bike);
 
@@ -125,8 +157,8 @@ public class OsmMap extends ActionBarActivity
 		}
 
 		bikesMarkersOverlay = new MarkerOverlay<BikeOverlayItem>(
-				getApplicationContext(), markers, mapView,
-				new BikeInfoWindow(mapView, getApplicationContext()));
+				getApplicationContext(), markers, mapView, new BikeInfoWindow(
+						mapView, getApplicationContext()));
 
 		mapView.getOverlays().add(bikesMarkersOverlay);
 	}
@@ -202,5 +234,69 @@ public class OsmMap extends ActionBarActivity
 				}
 			}
 		});
+	}
+
+	private void setSwitch()
+	{
+		btSwitch = (ToggleButton) findViewById(R.id.togglebutton);
+
+		btSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener()
+		{
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked)
+			{
+				if (isChecked)
+				{
+					mapView.getOverlays().remove(bikesMarkersOverlay);
+					mapView.invalidate();
+				}
+				else
+				{
+					mapView.getOverlays().add(bikesMarkersOverlay);
+					mapView.invalidate();
+				}
+			}
+		});
+
+	}
+	
+	private void setUpPointers()
+	{
+		stations = new ArrayList<Station>();
+		bikes = new ArrayList<Bike>();
+		
+		stations.add(new Station(new GeoPoint(45.890189, 11.034275),
+				"STAZIONE FF.SS. - Piazzale Orsi", 12));
+		stations.add(new Station(new GeoPoint(45.882221, 11.040483),
+				"OSPEDALE - Corso Verona", 12));
+		stations.add(new Station(new GeoPoint(45.886525, 11.044749),
+				"MUNICIPIO - Piazzetta Sichardt", 6));
+		stations.add(new Station(new GeoPoint(45.893571, 11.043891),
+				"MART - Corso Bettini", 6));
+		stations.add(new Station(new GeoPoint(45.866352, 11.019310),
+				"ZONA INDUSTRIALE - Viale Caproni", 6));
+		stations.add(new Station(new GeoPoint(45.892256, 11.039370),
+				"VIA PAOLI - Via Manzoni/Via Paoli", 12));
+		stations.add(new Station(new GeoPoint(45.840603, 11.009298),
+				"SACCO - Viale della Vittoria/Via Udine", 6));
+		stations.add(new Station(new GeoPoint(45.893120, 11.038846),
+				"SACCO - Viale della Vittoria/Via Udine", 12));
+		stations.add(new Station(new GeoPoint(45.883409, 11.072827),
+				"NORIGLIO - Via Chiesa San Martino", 6));
+		stations.add(new Station(new GeoPoint(45.904255, 11.044859),
+				"BRIONE - Piazza della Pace", 6));
+		stations.add(new Station(new GeoPoint(45.891021, 11.038729), "PIAZZA ROSMINI - via boh",
+				6));
+
+		bikes.add(new Bike(new GeoPoint(45.924255, 11.064859), "0"));
+		stations.get(0).setUsedSlots(11);
+		stations.get(0).addReport("segnalazione 1");
+		stations.get(0).addReport("segnalazione 2");
+		stations.get(0).addReport("segnalazione 3");
+		stations.get(1).setUsedSlots(12);
+		stations.get(2).setUsedSlots(6);
+		stations.get(3).setUsedSlots(5);
 	}
 }
