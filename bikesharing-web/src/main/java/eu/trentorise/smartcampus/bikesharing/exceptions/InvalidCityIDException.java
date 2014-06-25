51634b9b5d06873b274158fc27925bc6f97c8cd4
GabrieleMaurina <gabrielemaurina95@gmail.com>
2014-06-25 09:10:37 +0200

    deleting useless import

--- bikesharing-web/src/main/java/eu/trentorise/smartcampus/bikesharing/exceptions/InvalidCityIDException.java
+++ bikesharing-web/src/main/java/eu/trentorise/smartcampus/bikesharing/exceptions/InvalidCityIDException.java
@@ -1,7 +1,5 @@
 package eu.trentorise.smartcampus.bikesharing.exceptions;
 
-import java.util.Arrays;
-
 public class InvalidCityIDException extends Exception
 {
 	public InvalidCityIDException(String msg)
